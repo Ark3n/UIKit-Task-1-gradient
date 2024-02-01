60ebaa150b13b2c4ebf40e060689ebdb76a5c538
Arken <Arken.Sarsenov@outlook.com>
2024-02-01 14:03:00 +0500

    Initial Commit

---
+++ Maraforn-Task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maraforn-Task1
+//
+//  Created by Arken Sarsenov on 01.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
